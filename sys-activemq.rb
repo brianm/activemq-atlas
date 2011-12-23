e8345f3a45db71d25b52805af500e120f8f973d2
Brian McCallister <brianm@skife.org>
2011-12-23 14:01:49 -0700

    fix tab/space issue

--- sys-activemq.rb
+++ sys-activemq.rb
@@ -9,13 +9,13 @@ system "activemq" do
     cardinality: 3,
     base: "server",
     install: ["scratch:activemq-broker=@",
-	          "exec:sudo mkdir -p /opt/activemq/",
+			        "exec:sudo mkdir -p /opt/activemq/",
               "tgz:#{download}?to=/opt/activemq/&skiproot=apache-activemq-5.5.1",
               "erb:activemq.xml.erb > /opt/activemq/conf/activemq.xml",
-          	  "exec:sudo ln -s /opt/activemq/bin/activemq /etc/init.d/",
-          	  "exec:sudo update-rc.d activemq defaults",
-          	  "exec:sudo update-rc.d activemq enable",
-          	  "exec:sudo service start activemq"]
+              "exec:sudo ln -s /opt/activemq/bin/activemq /etc/init.d/",
+              "exec:sudo update-rc.d activemq defaults",
+              "exec:sudo update-rc.d activemq enable",
+              "exec:sudo service start activemq"]
   }
 end
 
