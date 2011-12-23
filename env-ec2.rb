3b1addef074dbcb54d4a505cdab5fc13a6615352
Brian McCallister <brianm@skife.org>
2011-12-23 13:59:00 -0700

    working on activemq atlas configs

---
+++ env-ec2.rb
@@ -0,0 +1,30 @@
+
+natty_useast_i386_ebs = "ami-e358958a"
+
+environment "ec2" do
+
+  listener "progress-bars"
+
+  listener "aws-config", {
+    ssh_ubuntu: "ubuntu@default",
+  }
+
+  system "security" do
+    service "activemq-group", base: "sec-group:activemq"
+  end
+
+  base "sec-group", {
+    provisioner: ["ec2-security-group:{base.fragment}", {
+                    ssh_from_outside:   "tcp 22 0.0.0.0/0",
+                    activemq_internal:  "tcp 80 activemq"
+                  }]
+  }
+
+  base "server", {
+    provisioner: ["ec2:#{natty_useast_i386_ebs}", {
+      instance_type: "m1.small",
+      security_group: "activemq"
+    }],
+    init: ["exec:sudo apt-get update", "apt:openjdk-6-jdk"]
+  }
+end
---
+++ sys-activemq.rb
@@ -0,0 +1,21 @@
+
+download = "http://apache.osuosl.org//activemq/apache-activemq/5.5.1/apache-activemq-5.5.1-bin.tar.gz"
+
+system "activemq" do
+
+  server "shell", base: "server"
+  
+  server "broker", {
+    cardinality: 3,
+    base: "server",
+    install: ["scratch:activemq-broker=@",
+	          "exec:sudo mkdir -p /opt/activemq/",
+              "tgz:#{download}?to=/opt/activemq/&skiproot=apache-activemq-5.5.1",
+              "erb:activemq.xml.erb > /opt/activemq/conf/activemq.xml",
+          	  "exec:sudo ln -s /opt/activemq/bin/activemq /etc/init.d/",
+          	  "exec:sudo update-rc.d activemq defaults",
+          	  "exec:sudo update-rc.d activemq enable",
+          	  "exec:sudo service start activemq"]
+  }
+end
+
